d82b01e1c1c444cf4460a25e8737b83efa707593
Gjermund Madsen <gjemad@gmail.com>
2018-07-05 23:12:52 +0200

    Initial commit

---
+++ WiFi Disabler/WiFi Disabler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace WiFi_Disabler
+{
+    public static class NetworkingExample
+    {
+        public static void Main()
+        {
+            NetworkChange.NetworkAddressChanged += new
+            NetworkAddressChangedEventHandler(AddressChangedCallback);
+            Console.WriteLine("Listening for address changes. Press any key to exit.");
+            Console.ReadLine();
+        }
+        static void AddressChangedCallback(object sender, EventArgs e)
+        {
+
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface n in adapters)
+            {
+                if (n.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    continue;
+                }
+                Console.WriteLine(n.Name + "  State: " + n.OperationalStatus);
+                if (n.NetworkInterfaceType == NetworkInterfaceType.Ethernet && n.OperationalStatus == OperationalStatus.Up)
+                {
+                    Console.WriteLine("Disabling WiFi...");
+                }
+            }
+        }
+    }
+}
